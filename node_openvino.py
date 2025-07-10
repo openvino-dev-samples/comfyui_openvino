8e77d6d6f9523acb39c408f71e39238b009864b0
ethan <ethan.yang@intel.com>
2025-07-09 18:17:56 -0700

    add model caching

--- node_openvino.py
+++ node_openvino.py
@@ -1,7 +1,7 @@
 import torch
 import openvino as ov
 import openvino.frontend.pytorch.torchdynamo.execute as ov_ex
-
+import os
 from comfy_api.torch_helpers import set_torch_compile_wrapper
 
 
@@ -10,11 +10,12 @@ class TorchCompileModelOpenVINO:
     def INPUT_TYPES(s):
         core = ov.Core()
         available_devices = core.available_devices
-
+        model_cache_option = ["OFF", "ON"]
         return {
             "required": {
                 "model": ("MODEL",),
                 "device": (available_devices,),
+                "model_cache": (model_cache_option,),
             },
         }
 
@@ -24,17 +25,28 @@ class TorchCompileModelOpenVINO:
     CATEGORY = "OpenVINO"
     EXPERIMENTAL = True
 
-    def patch(self, model, device):
-        options = {"device": device}
+    def patch(self, model, device, model_cache):
+        model_cache_option = {"OFF": False, "ON": True}
+        cache_path = os.path.join(os.getcwd(), "openvino_model_cache")
+        options = {
+            "device": device,
+            "model_caching": model_cache_option[model_cache],
+            "cache_dir": cache_path,
+        }
         torch._dynamo.reset()
         ov_ex.compiled_cache.clear()
         ov_ex.req_cache.clear()
         ov_ex.partitioned_modules.clear()
         m = model.clone()
-        set_torch_compile_wrapper(m,
-                                  backend="openvino",
-                                  options=options,
-                                  )
+
+        if model_cache_option[model_cache]:
+            print(f"Using the model cache from {cache_path}.")
+
+        set_torch_compile_wrapper(
+            m,
+            backend="openvino",
+            options=options,
+        )
         return (m,)
 
 
